8b75150052d502f0b84059a41b9f679ec4169066
Alokkodzz <alok635790@gmail.com>
2026-04-15 13:22:41 +0000

    updated the region

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "alokk-mlopss"
+    bucket = "alokk-mlopss-tf-backend"
     key    = "State/terraform.tfstate"
-    region = "ap-southeast-2"
+    region = "ap-south-1"
   }
 }
