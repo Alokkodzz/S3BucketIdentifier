b6b110feaed037e471fd4d81cb4c286665f07c03
Alokkodzz <alok635790@gmail.com>
2026-04-19 08:47:00 +0000

    updated the code to use RandomForest algorithem

--- ml/train.py
+++ ml/train.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -19,8 +19,13 @@ vectorizer = TfidfVectorizer(analyzer='char', ngram_range=(2,4))
 X = vectorizer.fit_transform(df["filename"])
 y = df["bucket"]
 
-model = LogisticRegression(max_iter=200)
+model = RandomForestClassifier(
+    n_estimators=200,     # number of trees
+    max_depth=None,       # let trees grow fully
+    random_state=42,
+    n_jobs=-1             # use all CPU cores
+)
 model.fit(X, y)
 
-joblib.dump(model, "../model/model.joblib")
-joblib.dump(vectorizer, "../model/vectorizer.joblib")
+joblib.dump(model, "../model/model.pkl")
+joblib.dump(vectorizer, "../model/vectorizer.pkl")
